40c32ad6313e17f4c89c6d578c0d951ad2554ff8
Jaggernaut09 <mrhomka.com@gmail.com>
2020-06-19 19:45:29 +0500

    add tests

---
+++ NUnitTestProject/UnitTest1.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using Cgame.Core;
+
+namespace NUnitTestProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void TestCollider()
+        {
+
+        }
+    }
+}
